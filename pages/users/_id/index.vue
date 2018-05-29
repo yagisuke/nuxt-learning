b41939d7019851383bbb95ff3916a11933a91cf2
yagisuke <yagisuke.0209@gmail.com>
2018-05-30 00:27:06 +0900

    add users detail page

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    {{ $route.params.id }}
+  </section>
+</template>
+
+<style>
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
