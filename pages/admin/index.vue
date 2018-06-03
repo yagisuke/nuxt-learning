260ac7ad860b233a47599234563d64374706914e
yagisuke <yagisuke.0209@gmail.com>
2018-06-03 19:33:00 +0900

    add new post page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="admin-page">
-    <h1>admin page!</h1>
-    <p>text text text text text text text</p>
-    <p>text text text text text text text</p>
-    <p>text text text text text text text</p>
-    <p>text text text text text text text</p>
-    <p>text text text text text text text</p>
+    <section class="new-post">
+      <button
+        @click="$router.push('/admin/new-post')"
+      >
+        Create Post
+      </button>
+    </section>
+    <section class="existing-posts">
+      <h1>Existing Posts</h1>
+      <PostList />
+    </section>
   </div>  
 </template>
 
+<script>
+import PostList from '@/components/Posts/PostList'
+
+export default {
+  components: {
+    PostList
+  }
+}
+</script>
+
 <style scoped>
 .admin-page {
+  padding: 20px;
+}
+
+.new-post {
   text-align: center;
-  padding: 30px;
+  border-bottom: 2px solid lightgray;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
 }
 </style>
 
