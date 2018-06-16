75a0f95f77cf34be0d9ad3ce64fe96da1dc4edf1
yagisuke <yagisuke.0209@gmail.com>
2018-06-16 16:21:43 +0900

    use promise

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,31 +9,39 @@ export default {
   components: {
     PostList
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: '1',
-            title: 'first post',
-            previewText: 'this is our first post',
-            thumbnail: 'http://marimek-prod.item.photo/product/52_1_5263009231/52_1_5263009231_main.jpg'
-          },
-          {
-            id: '2',
-            title: '2nd post',
-            previewText: 'this is our first post',
-            thumbnail: 'http://marimek-prod.item.photo/product/52_1_5263009231/52_1_5263009231_main.jpg'
-          },
-          {
-            id: '3',
-            title: '3rd post',
-            previewText: 'this is our first post',
-            thumbnail: 'http://marimek-prod.item.photo/product/52_1_5263009231/52_1_5263009231_main.jpg'
-          }
-        ]
-      })
-    }, 1500)
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: '1',
+              title: '1st post',
+              previewText: 'this is our first post',
+              thumbnail: 'http://marimek-prod.item.photo/product/52_1_5263009231/52_1_5263009231_main.jpg'
+            },
+            {
+              id: '2',
+              title: '2nd post',
+              previewText: 'this is our first post',
+              thumbnail: 'http://marimek-prod.item.photo/product/52_1_5263009231/52_1_5263009231_main.jpg'
+            },
+            {
+              id: '3',
+              title: '3rd post',
+              previewText: 'this is our first post',
+              thumbnail: 'http://marimek-prod.item.photo/product/52_1_5263009231/52_1_5263009231_main.jpg'
+            }
+          ]
+        })
+      }, 1000)
+    })
+    .then(data => {
+      return data
+    })
+    .catch(e => {
+      context.error(new Error())
+    })
   }
 }
 </script>
