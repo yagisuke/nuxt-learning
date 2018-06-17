f418e396e54bc07d44185c05006f719e72453525
yagisuke <yagisuke.0209@gmail.com>
2018-06-18 01:29:02 +0900

    add functions

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,8 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~plugins/core-components.js'
+    '~plugins/core-components.js',
+    '~plugins/date-filter.js'
   ],
 
   /*
