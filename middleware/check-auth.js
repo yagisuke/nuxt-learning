54e154b853fe76c45dfb1cdf22b88c3bc460a72f
yagisuke <yagisuke.0209@gmail.com>
2018-06-24 16:08:23 +0900

    hello

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,6 +1,4 @@
 export default function (context) {
   console.log(`[Middleware]: check-auth`)
-  if (process.client) {
-    context.store.dispatch('initAuth')
-  }
+  context.store.dispatch('initAuth', context.req)
 }
